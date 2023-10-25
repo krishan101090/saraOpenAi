ee79a6cdd48ae6dc0ce20c5553f4234d8f3b0656
krishan mohan <>
2023-10-25 13:47:47 +0530

    init

---
+++ config/notionurls.ts
@@ -0,0 +1,6 @@
+export const urls = [
+  'https://www.operabase.com/en',
+  'https://www.operabase.com/pro/companies/en',
+  'https://www.operabase.com/proaudience/en',
+  'https://www.operabase.com/casting/en',
+];
