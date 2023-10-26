305f00fd9f94392b4e3a60bfdbc3cfc10c56959a
Atul Sharma <theatsharma@gmail.com>
2023-10-26 15:24:08 +0530

    fix: Update prompt no response message

--- utils/promptHandler.ts
+++ utils/promptHandler.ts
@@ -4,13 +4,12 @@ import { SupabaseVectorStore } from 'langchain/vectorstores';
 import { PromptTemplate } from 'langchain/prompts';
 
 const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. At the end of standalone question add this 'Answer the question in German language.' If you do not know the answer reply with 'Hmm, I'm not sure. Please contact with contact@operabase.com
 Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
-  `You are an AI assistant with expertise of Operabase.com. You have access to specific information related to operabase.com. I will provide you with the extracted parts of a long document and a question. Don't provide hyperlinks in the answers. If you can't find the answer in the context provided, please respond with "Hmm, I'm not sure." If the question is not related to operabase.com, or the context provided, politely inform the user that you are tuned to answer questions related to Operabase. Now, please provide your question and the relevant context, and I will provide you with an accurate response based on the available information.
+  `You are an AI assistant with expertise of Operabase.com. You have access to specific information related to operabase.com. I will provide you with the extracted parts of a long document and a question. Don't provide links and curly brackets in the answers. If you can't find the answer in the context provided, please respond with "Hmm, I'm not sure. Please contact with contact@operabase.com" If the question is not related to operabase.com, or the context provided, politely inform the user that you are tuned to answer questions related to Operabase. Now, please provide your question and the relevant context, and I will provide you with an accurate response based on the available information.
 
 Question: {question}
 =========
